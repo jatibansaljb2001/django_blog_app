35b5945b62782c6a7fd37af6f71a252d73fd5022
jatibansaljb2001 <62650742+jatibansaljb2001@users.noreply.github.com>
2021-07-16 11:22:30 +0530

    updated settings.py

--- my_site/settings.py
+++ my_site/settings.py
@@ -135,5 +135,5 @@ EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 EMAIL_PORT = 587
-EMAIL_HOST_USER = os.environ('EMAIL_USER')
-EMAIL_HOST_PASSWORD = os.environ('EMAIL_PASSWORD')
+EMAIL_HOST_USER = "jatinbansaljb2001@gmail.com"
+EMAIL_HOST_PASSWORD = "Jatin@2001"
